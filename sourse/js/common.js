d03d4d39aa287fcaf754f7219205d9743fa44c51
Pavel Khalunin <wol1414@gmail.com>
2019-05-05 19:32:02 +0300

    add 2 blocks

--- sourse/js/common.js
+++ sourse/js/common.js
@@ -353,7 +353,7 @@ var JSCCommon = {
 		$(window).on('load', function() {
 
 			circle();
-		}
+		});
 				function circle() {
 					$(".swiper-pagination-bullet").find("svg").remove();
 					$(".swiper-pagination-bullet-active ").circliful({
